49293dfb0a655d92407c8a49d1da8a91f98551e6
Juan Rivera Vargas <jrivera.v@outlook.com>
2020-04-25 01:36:53 -0400

    include image in public folder

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,12 +9,23 @@ export default function Home() {
       <Head>
         <title>Ariverak - Curriculum</title>
         <meta name="viewport" content="width=device-width;user-scalable=no" />
+        <meta property="og:image" content="/juan-rivera-vargas.jpg" />
+        <meta
+          name="og:title"
+          property="og:title"
+          content="Curriculum - Juan Rivera Vargas"
+        />
+        <meta
+          name="og:description"
+          property="og:description"
+          content="Este curriculum esta hecho 100% a mano usando nextjs"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <Background>
           <Layout
-            imageUrl="https://avatars3.githubusercontent.com/u/29168528?s=400&u=5513d95e49917ca3c6b070bcdf2a40ecd53ab485&v=4"
+            imageUrl="/juan-rivera-vargas.jpg"
             about={`
             Soy una persona pro activa, me gustan las nuevas tecnologías y los desafíos, e desarrollado variadas soluciones de software desde aplicaciones móviles, web y servicios backend, me desenvuelvo muy bien en el frontend como en el backend.
             No obstante siempre estoy aprendiendo cosas nuevas.        
