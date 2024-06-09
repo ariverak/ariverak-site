7a00f5139dd417038a1a5530be5393b7c0513084
Andrés Rivera <jrivera.v@outlook.com>
2024-06-09 13:06:03 -0400

    feat: add education and experience

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Avatar, Divider, User } from '@nextui-org/react'
+import { Avatar, Divider } from '@nextui-org/react'
+import { GraduationCap, Laptop } from 'lucide-react'
 
 export interface ContentProps {
   title: string
@@ -30,11 +31,25 @@ const Content: React.FC<ContentProps> = ({
       <h2 className="text-2xl font-semibold">{subtitle}</h2>
       <Divider className="my-4" />
       {/* education */}
-      <h3 className="mb-2 text-lg font-semibold">Educación</h3>
-
+      <h3 className="mb-2 flex items-center text-lg font-semibold">
+        <GraduationCap size={24} className="mr-2" />
+        Educación
+      </h3>
+      {education.map((edu, index) => (
+        <div key={index} className="my-4">
+          <h4 className="text-lg font-bold">{edu.university}</h4>
+          <p className="text-sm font-semibold">{edu.career}</p>
+          <p className="text-xs text-gray-500">
+            {edu.yearsBetween[0]} - {edu.yearsBetween[1]}
+          </p>
+        </div>
+      ))}
       <Divider className="my-4" />
       {/* profesional experience */}
-      <h3 className="mb-2 text-lg font-semibold">Experiencia Profesional</h3>
+      <h3 className="mb-2 flex items-center text-lg font-semibold">
+        <Laptop size={24} className="mr-2" />
+        Experiencia Profesional
+      </h3>
       {experience.map((exp, index) => (
         <div key={index} className="my-4 flex items-center">
           <Avatar
